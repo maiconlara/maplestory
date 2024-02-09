9624f2d9bc977a77cb4a5ef235ab46b186d41fd1
maiconlara <maiconluan.lara@yahoo.com.br>
2024-02-09 09:20:16 -0300

    feat: adding fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        patrick: ["Patrick Hand", "sans-serif"],
+        quick: ["Quicksand", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import removeConsole from "vite-plugin-remove-console";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+  plugins: [react(), removeConsole()],
+});
