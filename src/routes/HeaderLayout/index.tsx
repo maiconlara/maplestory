3e6a4241351361fa5d72713cd3832e740d001946
maiconlara <maiconluan.lara@yahoo.com.br>
2024-02-09 09:21:12 -0300

    feat: adding header layout

---
+++ src/routes/HeaderLayout/index.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Header from "./header";
+
+const HeaderLayout = () => {
+  return (
+    <div className="flex flex-col max-w-[100vw] w-full min-h-[100vh] overflow-hidden justify-center items-center bg-[#32858d] ">
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
+export default HeaderLayout;
