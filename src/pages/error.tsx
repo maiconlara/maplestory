7179c5a27921d6733514da4c0046bc770189310b
maiconlara <maiconluan.lara@yahoo.com.br>
2024-02-14 13:18:37 -0300

    refactor: centering error page

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,7 +1,6 @@
-
 const Error = () => {
   return (
-    <div id="error-page">
+    <div id="error-page" className="flex flex-col flex-1 justify-center items-center gap-2 ">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p></p>
