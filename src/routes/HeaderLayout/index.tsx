85a1d9128db2625d1370462d04253dce6f9c441a
maiconlara <maiconluan.lara@yahoo.com.br>
2024-02-14 13:36:54 -0300

    refactor: changing layout bg color

--- src/routes/HeaderLayout/index.tsx
+++ src/routes/HeaderLayout/index.tsx
@@ -3,7 +3,7 @@ import Header from "./header";
 
 const HeaderLayout = () => {
   return (
-    <div className="flex flex-col max-w-[100vw] w-full min-h-[100vh] overflow-hidden justify-start items-center bg-[#32858d] ">
+    <div className="flex flex-col max-w-[100vw] w-full min-h-[100vh] overflow-hidden justify-start items-center bg-[#d6edff] ">
       <Header />
       <Outlet />
     </div>
