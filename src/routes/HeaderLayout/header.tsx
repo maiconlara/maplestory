5f6b0e15ef3cb3666e51b077c06448e9f85c1896
maiconlara <maiconluan.lara@yahoo.com.br>
2024-02-15 15:16:15 -0300

    refactor: adding hover in header button

--- src/routes/HeaderLayout/header.tsx
+++ src/routes/HeaderLayout/header.tsx
@@ -4,19 +4,19 @@ const Header = () => {
   return (
     <div className="flex px-24 max-w-full max-h-[115px] min-h-[115px] w-full h-full gap-20 bg-white/40 fixed top-0 justify-start items-center">
       <img src={logoLeaf} className="h-16" />
-      <p className="font-poppins uppercase font-semibold text-lg cursor-pointer">
+      <p className="font-poppins uppercase font-semibold text-lg cursor-pointer hover:text-[#d1400b] transition-colors">
         Download
       </p>
-      <p className="font-poppins uppercase font-semibold text-lg cursor-pointer">
+      <p className="font-poppins uppercase font-semibold text-lg cursor-pointer hover:text-[#d1400b] transition-colors">
         Register
       </p>
-      <p className="font-poppins uppercase font-semibold text-lg cursor-pointer">
+      <p className="font-poppins uppercase font-semibold text-lg cursor-pointer hover:text-[#d1400b] transition-colors">
         Rankings
       </p>
-      <p className="font-poppins uppercase font-semibold text-lg cursor-pointer">
+      <p className="font-poppins uppercase font-semibold text-lg cursor-pointer hover:text-[#d1400b] transition-colors">
         Vote
       </p>
-      <p className="font-poppins uppercase font-semibold text-lg cursor-pointer">
+      <p className="font-poppins uppercase font-semibold text-lg cursor-pointer hover:text-[#d1400b] transition-colors">
         Premium
       </p>
     </div>
