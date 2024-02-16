44a75cc64de1e22754b5a006ded0379daac04abb
maiconlara <maiconluan.lara@yahoo.com.br>
2024-02-16 16:15:13 -0300

    refactor: refactoring header buttons

--- src/routes/HeaderLayout/header.tsx
+++ src/routes/HeaderLayout/header.tsx
@@ -1,24 +1,43 @@
 import logoLeaf from "../../assets/leafLogo.png";
 
+const headerButtons = [
+  {
+    text: "Download",
+    link: "/download",
+  },
+  {
+    text: "Register",
+    link: "/register",
+  },
+  {
+    text: "Rankings",
+    link: "/rankings",
+  },
+  {
+    text: "Vote",
+    link: "/vote",
+  },
+  {
+    text: "Premium",
+    link: "/premium",
+  },
+];
+
 const Header = () => {
   return (
     <div className="flex px-24 max-w-full max-h-[115px] min-h-[115px] w-full h-full gap-20 bg-white/40 fixed top-0 justify-start items-center">
       <img src={logoLeaf} className="h-16" />
-      <p className="font-poppins uppercase font-semibold text-lg cursor-pointer hover:text-[#d1400b] transition-colors">
-        Download
-      </p>
-      <p className="font-poppins uppercase font-semibold text-lg cursor-pointer hover:text-[#d1400b] transition-colors">
-        Register
-      </p>
-      <p className="font-poppins uppercase font-semibold text-lg cursor-pointer hover:text-[#d1400b] transition-colors">
-        Rankings
-      </p>
-      <p className="font-poppins uppercase font-semibold text-lg cursor-pointer hover:text-[#d1400b] transition-colors">
-        Vote
-      </p>
-      <p className="font-poppins uppercase font-semibold text-lg cursor-pointer hover:text-[#d1400b] transition-colors">
-        Premium
-      </p>
+
+      {headerButtons.map((item, index) => {
+        return (
+          <p
+            className="font-poppins uppercase font-semibold text-lg cursor-pointer hover:text-[#d1400b] transition-colors"
+            key={index}
+          >
+            {item.text}
+          </p>
+        );
+      })}
     </div>
   );
 };
