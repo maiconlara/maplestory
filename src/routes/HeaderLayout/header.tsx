0c1658fb1f387cab933646dd66524b484dbc662f
maiconlara <maiconluan.lara@yahoo.com.br>
2024-02-20 15:02:59 -0300

    refactor: layout changes in header

--- src/routes/HeaderLayout/header.tsx
+++ src/routes/HeaderLayout/header.tsx
@@ -1,5 +1,8 @@
+import { IconBrandDiscordFilled, IconUserFilled } from "@tabler/icons-react";
 import { useNavigate } from "react-router-dom";
 import logoLeaf from "../../assets/leafLogo.png";
+import TextButton from "./text-button";
+import HeaderButton from "./header-button";
 
 const headerButtons = [
   {
@@ -28,24 +31,30 @@ const Header = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex px-24 max-w-full max-h-[115px] min-h-[115px] w-full h-full gap-20 bg-white/40 fixed top-0 justify-start items-center">
-      <img
-        src={logoLeaf}
-        className="h-16 cursor-pointer select-none"
-        onClick={() => navigate("/")}
-      />
+    <div className="flex px-24 max-w-full max-h-[115px] min-h-[115px] w-full h-full bg-white/40 fixed top-0 justify-between items-center">
+      <div className="flex flex-row gap-20 items-center justify-center">
+        <img
+          src={logoLeaf}
+          className="h-16 cursor-pointer select-none"
+          onClick={() => navigate("/")}
+        />
 
-      {headerButtons.map((item, index) => {
-        return (
-          <p
-            className="font-poppins uppercase font-semibold text-lg cursor-pointer hover:text-[#d1400b] transition-colors select-none"
-            key={index}
-            onClick={() => navigate(`/${item.link}`)}
-          >
-            {item.text}
-          </p>
-        );
-      })}
+        {headerButtons.map((item, index) => {
+          return <TextButton key={index} text={item.text} link={item.link} />;
+        })}
+      </div>
+
+      <div className="flex flex-row items-center justify-center gap-12">
+        <div className="flex items-center justify-center font-poppins font-semibold h-[40px] w-[160px] bg-[#238636] rounded-full text-white ">
+          {`${Math.floor(Math.random() * (107 - 100 + 1) + 100)} Online`}
+        </div>
+        <HeaderButton
+          Icon={IconBrandDiscordFilled}
+          text="Discord"
+          color="#6d84d7"
+        />
+        <HeaderButton Icon={IconUserFilled} text="Account" color="#333333" />
+      </div>
     </div>
   );
 };
