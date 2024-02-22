e70797c554eaa4f87d19d28052a5aa8c1dc5a1f1
maiconlara <maiconluan.lara@yahoo.com.br>
2024-02-22 14:46:28 -0300

    feat: adding feature section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,12 @@
+import {
+  IconBalloon,
+  IconBan,
+  IconCoinBitcoin,
+  IconConfetti,
+  IconMoodTongueWink,
+  IconRocket,
+  IconTrendingUp,
+} from "@tabler/icons-react";
 import DefaultButton from "../components/default-button";
 import { useTabTitle } from "../hooks/useTabTitle";
 
@@ -14,16 +23,79 @@ const Home = () => {
           </p>
         </div>
         <div className="flex flex-row gap-6">
-          <DefaultButton text="get started" />
-          <DefaultButton text="read more" />
+          <DefaultButton text="get started" link="download" />
+          <DefaultButton text="read more" link="#overview" useHref />
         </div>
       </div>
       <div className="flex flex-none  w-full h-1 bg-white" />
 
-      <div className="flex flex-col w-full px-24 h-full mt-16 justify-center items-center">
-        <p className="font-poppins font-semibold text-4xl ">
-          Overview
-        </p>
+      <div className="flex flex-col w-full max-w-full px-24 h-full mt-16 justify-center items-center gap-20 mb-6">
+        <div className="flex flex-col w-full items-center justify-center gap-4 ">
+          <p id="overview" className="font-poppins font-semibold text-4xl ">
+            Overview
+          </p>
+          <p className="font-poppins font-medium text-xl text-center  ">
+            MuLung Story is a MapleStory private server that is dedicated to
+            providing a unique and challenging experience. We are a low-rate
+            server that is focused on providing a nostalgic and challenging
+            experience for our players. We are focusing on strong a late game
+            content with a higher level cap and bigger line damages.
+          </p>
+        </div>
+        <div className="flex flex-col w-full items-center justify-center gap-4 ">
+          <p className="font-poppins font-semibold text-4xl ">Features</p>
+          <div className="flex flex-col gap-6 w-full justify-start ">
+            <div className="flex flex-row gap-4 w-full items-center justify-start ">
+              <IconTrendingUp size={24} />
+              <p className="font-poppins font-semibold text-lg">
+                Game Progression:{" "}
+                <span className="font-normal font-poppins">
+                  {" "}
+                  3.2x EXP, 6x Quests, 2x DROP and 8x MESOS;{" "}
+                </span>{" "}
+              </p>
+            </div>
+
+            <div className="flex flex-row gap-4 w-full items-center justify-start ">
+              <IconRocket size={24} />
+              <p className="font-poppins font-semibold text-lg">
+                Maximum Level 220;
+              </p>
+            </div>
+
+            <div className="flex flex-row gap-4 w-full items-center justify-start ">
+              <IconConfetti size={24} />
+              <p className="font-poppins font-semibold text-lg">
+                All party quests available;
+              </p>
+            </div>
+
+            <div className="flex flex-row gap-4 w-full items-center justify-start ">
+              <IconBalloon size={24} />
+              <p className="font-poppins font-semibold text-lg">
+                Seasonal hosted events;
+              </p>
+            </div>
+            <div className="flex flex-row gap-4 w-full items-center justify-start ">
+              <IconBan size={24} />
+              <p className="font-poppins font-semibold text-lg">
+                Functional anti-cheat;
+              </p>
+            </div>
+            <div className="flex flex-row gap-4 w-full items-center justify-start ">
+              <IconCoinBitcoin size={24} />
+              <p className="font-poppins font-semibold text-lg">
+                Own Cryptocurrency;
+              </p>
+            </div>
+            <div className="flex flex-row gap-4 w-full items-center justify-start ">
+              <IconMoodTongueWink size={24} />
+              <p className="font-poppins font-semibold text-lg">
+                And much more!
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
