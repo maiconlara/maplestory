0e7de6a5e76d380de2bf61e91cd98c7f4ba11e09
maiconlara <maiconluan.lara@yahoo.com.br>
2024-02-26 14:39:42 -0300

    refactor: layout footer

--- src/routes/HeaderLayout/index.tsx
+++ src/routes/HeaderLayout/index.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./header";
+import Footer from "./footer";
 
 const HeaderLayout = () => {
   return (
-    <div className="flex flex-col max-w-[100vw] w-full min-h-[100vh] overflow-hidden justify-start items-center bg-[#d6edff] ">
+    <div className="flex flex-col relative max-w-[100vw] w-full min-h-[100vh] overflow-hidden justify-start items-center bg-[#d6edff] ">
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
