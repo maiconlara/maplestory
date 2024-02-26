1a9358621c8896be460b6da929fe7db3e77b6295
maiconlara <maiconluan.lara@yahoo.com.br>
2024-02-26 14:39:13 -0300

    feat: adding footer component

---
+++ src/routes/HeaderLayout/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="flex px-24 max-w-full max-h-[115px] min-h-[115px] w-full h-full bg-[#232552] sticky bottom-0 justify-between items-center">
+      <div className="flex flex-row items-center justify-center gap-12 w-full text-white font-poppins">
+        <p>
+          © 2024 NEXON Korea Corporation and NEXON America Inc. All Rights
+          Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Footer;
