501a0f5db5704efc6cee2cec376838b3529f28c7
maiconlara <maiconluan.lara@yahoo.com.br>
2024-02-26 14:40:09 -0300

    refactor: adding padding to allign with footer

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -14,7 +14,7 @@ const Home = () => {
   useTabTitle("Home");
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full">
+    <div className="flex flex-col items-center justify-center w-full h-full pb-12">
       <div className="flex flex-col px-24 gap-12 bg-hero bg-cover w-full min-h-[700px] h-full bg-no-repeat bg-center overflow-hidden justify-center">
         <div className="flex flex-col gap-3 pt-6">
           <p className="font-poppins font-medium text-4xl  ">MuLung Story</p>
