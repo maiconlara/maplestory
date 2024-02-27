6165d2d98ca084d3ef16eca4d466f23c05ce369a
maiconlara <maiconluan.lara@yahoo.com.br>
2024-02-27 10:44:56 -0300

    refactor: componentizing overview

---
+++ src/components/home/overview-section.tsx
@@ -0,0 +1,18 @@
+const OverViewSection = () => {
+  return (
+    <div className="flex flex-col w-full items-center justify-center gap-6 ">
+      <p id="overview" className="font-poppins font-semibold text-4xl ">
+        Overview
+      </p>
+      <p className="font-poppins font-medium text-xl text-center  ">
+        MuLung Story is a MapleStory private server that is dedicated to
+        providing a unique and challenging experience. We are a low-rate server
+        that is focused on providing a nostalgic and challenging experience for
+        our players. We are focusing on strong a late game content with a higher
+        level cap and bigger line damages.
+      </p>
+    </div>
+  );
+};
+
+export default OverViewSection;
