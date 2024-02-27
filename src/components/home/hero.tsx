660d44bbbc1264d190531013087fea5b19d08556
maiconlara <maiconluan.lara@yahoo.com.br>
2024-02-27 10:45:02 -0300

    refactor: componentizing hero

---
+++ src/components/home/hero.tsx
@@ -0,0 +1,19 @@
+import DefaultButton from "../default-button";
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col px-24 gap-12 bg-hero bg-cover w-full min-h-[700px] h-full bg-no-repeat bg-center overflow-hidden justify-center">
+      <div className="flex flex-col gap-3 pt-6">
+        <p className="font-poppins font-medium text-4xl  ">MuLung Story</p>
+        <p className="font-poppins font-medium text-4xl ">
+          The new era Maple Server!
+        </p>
+      </div>
+      <div className="flex flex-row gap-6">
+        <DefaultButton text="get started" link="download" />
+        <DefaultButton text="read more" link="#overview" useHref />
+      </div>
+    </div>
+  );
+};
+export default Hero;
