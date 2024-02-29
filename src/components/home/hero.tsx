226c074926d6d1ab865969693480bb5dea425f44
maiconlara <maiconluan.lara@yahoo.com.br>
2024-02-29 14:38:37 -0300

    refactor: making hero screen size

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -2,7 +2,7 @@ import DefaultButton from "../default-button";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col px-32 gap-12 bg-hero bg-cover w-full min-h-[700px] h-full bg-no-repeat bg-center overflow-hidden justify-center">
+    <div className="flex flex-col px-32 gap-12 bg-hero bg-cover w-full min-h-screen h-full bg-no-repeat bg-center overflow-hidden justify-center">
       <div className="flex flex-col gap-3 pt-6">
         <p className="font-poppins font-medium text-4xl  ">MuLung Story</p>
         <p className="font-poppins font-medium text-4xl ">
