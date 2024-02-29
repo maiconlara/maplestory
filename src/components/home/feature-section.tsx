61164de841ee325f4c95404147f15556010872b2
maiconlara <maiconluan.lara@yahoo.com.br>
2024-02-29 14:38:28 -0300

    refactor: changing every aspect of feature

--- src/components/home/feature-section.tsx
+++ src/components/home/feature-section.tsx
@@ -13,7 +13,7 @@ import asset2 from "../../assets/asset2.png";
 
 const FeatureSection = () => {
   return (
-    <div className="flex flex-col w-full items-center justify-center gap-6 ">
+    <div className="flex flex-col w-[60vw] items-center justify-center gap-12 ">
       <p className="font-poppins font-semibold text-4xl ">Features</p>
 
       <div className="flex flex-row w-full items-center">
@@ -32,33 +32,53 @@ const FeatureSection = () => {
           <div className="flex flex-row gap-4 w-full items-center justify-start ">
             <IconRocket size={24} />
             <p className="font-poppins font-semibold text-lg">
-              Maximum Level 220;
+              Maximum Level 220:
+              <span className="font-normal font-poppins">
+                {" "}
+                To make the game more competitive;{" "}
+              </span>{" "}
             </p>
           </div>
 
           <div className="flex flex-row gap-4 w-full items-center justify-start ">
             <IconConfetti size={24} />
             <p className="font-poppins font-semibold text-lg">
-              All party quests available;
+              All party quests available:
+              <span className="font-normal font-poppins">
+                {" "}
+                Creating a friendly environment;{" "}
+              </span>{" "}
             </p>
           </div>
 
           <div className="flex flex-row gap-4 w-full items-center justify-start ">
             <IconBalloon size={24} />
             <p className="font-poppins font-semibold text-lg">
-              Seasonal hosted events;
+              Seasonal hosted events:
+              <span className="font-normal font-poppins">
+                {" "}
+                Making everyone happy and beauty;{" "}
+              </span>{" "}
             </p>
           </div>
           <div className="flex flex-row gap-4 w-full items-center justify-start ">
             <IconBan size={24} />
             <p className="font-poppins font-semibold text-lg">
-              Functional anti-cheat;
+              Functional anti-cheat:
+              <span className="font-normal font-poppins">
+                {" "}
+                So no one could outlevel you;{" "}
+              </span>{" "}
             </p>
           </div>
           <div className="flex flex-row gap-4 w-full items-center justify-start ">
             <IconCoinBitcoin size={24} />
             <p className="font-poppins font-semibold text-lg">
-              Own Cryptocurrency;
+              Own Cryptocurrency:
+              <span className="font-normal font-poppins">
+                {" "}
+                I heard people like that NFT thing;{" "}
+              </span>{" "}
             </p>
           </div>
           <div className="flex flex-row gap-4 w-full items-center justify-start ">
@@ -67,10 +87,10 @@ const FeatureSection = () => {
           </div>
           <DefaultButton text="lets play!" link="register" />
         </div>
-        <div className="flex justify-center items-center w-full h-full">
+        <div className="flex justify-end items-center w-[40%] h-full">
           <div className="flex relative w-[300px] h-[200px] select-none">
-            <img src={asset1} className=" absolute bottom-0 left-24   " />
-            <img src={asset2} className=" absolute bottom-0 left-12   " />
+            <img src={asset1} className=" absolute bottom-0 right-0   " />
+            <img src={asset2} className=" absolute bottom-0 right-20   " />
           </div>
         </div>
       </div>
