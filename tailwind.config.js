14814762b6ee7731ec2d29689d13c878942c45c7
maiconlara <maiconluan.lara@yahoo.com.br>
2024-03-05 10:55:05 -0300

    refactor: changing breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+
+      md: "768px",
+
+      lg: "1024px",
+
+      xl: "1280px",
+
+      "2xl": "1536px",
+
+      "3xl": "1600px",
+
+      "4xl": "2560px",
+    },
     extend: {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
